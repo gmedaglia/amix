173d9e650dff40903518d1019644a554f20d6c38
Germán Medaglia <german.medaglia@gmail.com>
2026-03-05 18:15:02 -0300

    Added cache for GET sale endpoint.

--- routes/api.php
+++ routes/api.php
@@ -5,9 +5,12 @@ use App\Http\Controllers\ProductController;
 use App\Http\Controllers\SaleController;
 use App\Http\Controllers\ServiceController;
 use Illuminate\Support\Facades\Route;
+use Spatie\ResponseCache\Middlewares\CacheResponse;
+
+use function Illuminate\Support\days;
 
 Route::apiResource('clients', ClientController::class);
 Route::apiResource('products', ProductController::class);
 Route::apiResource('services', ServiceController::class);
 Route::apiResource('clients.sales', SaleController::class)->only('store');
-Route::apiResource('sales', SaleController::class)->only('show');
+Route::apiResource('sales', SaleController::class)->only('show')->middleware(CacheResponse::for(days(30)));
